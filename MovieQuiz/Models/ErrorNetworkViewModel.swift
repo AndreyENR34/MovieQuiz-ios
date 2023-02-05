599664a00500e470343ca827cb6e75b5af24d0c3
Andrey Nikolaev <Andreynick2006@ya.ru>
2023-02-06 00:06:35 +1200

    Добавил работу с сетью, добавил алерт ошибки загрузки данных из сети интернет с возможностью повторной загрузки по нажатию клавиши на алерте .

---
+++ MovieQuiz/Models/ErrorNetworkViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorNetwork.swift
+//  MovieQuiz
+//
+//  Created by Andrey Nikolaev on 01.02.2023.
+//
+
+import Foundation
+struct ErrorNetworkViewModel {
+    let title: String
+    let buttonText: String
+}
